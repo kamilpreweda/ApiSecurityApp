e2f09a8b68d699306d554ba3c96ffa9c98ba9333
Kamil Preweda <preweda.kamil@o2.pl>
2022-09-02 17:27:19 +0200

    Created template and record for Authentication

---
+++ ApiSecurity/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSecurity.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    public record AuthenticationData(string? UserName, string? Password);
+    [HttpPost("token")]
+    public ActionResult<string> Authenticate([FromBody] Data data)
+    {
+
+    }
+} 
